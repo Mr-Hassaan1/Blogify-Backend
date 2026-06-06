e82419c7124dab60696dc3bc4d2806ba687739da
Mr-Hassaan1 <md.hassaan.tahir@gmail.com>
2026-06-06 19:57:25 +0500

    added frontend vercel link in backend server route

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://blogifypk.vercel.app",
     credentials: true,
   })
 );
